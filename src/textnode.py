c8c5c1298713f5b0855e893f0268f97eb8cfb324
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-12-10 11:36:46 -0500

    first part kinda done

---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class TextType(Enum):
+    def __init__(self):
+        PLAIN = 0
+        BOLD = 1
+        ITALIC = 2
+        CODE = 3
+        LINKS = 4
+        IMAGES = 5
+
+class TextNode:
+    def __init__()
