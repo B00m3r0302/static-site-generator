8654f5f03507416dc73a718b250fbf1822c51608
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-12-19 14:00:50 -0500

    recursive function for file copy added in main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-print(TextNode("lalala", TextType.images, "https://circlebuddy.com"))
+
+def copy_to(source, destination):
+    if not os.path.exists(source):
+        raise Exception("Can't find the source directory....not much to do here....")
+
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    os.makedirs(destination)
+
+    # copy contents recursively
+    for item in os.listdir(source):
+        src_path = os.path.join(source, item)
+        dst_path = os.path.join(destination, item)
+
+        if os.path.isdir(src_path):
+            copy_to(src_path, dst_path)
+        else:
+            shutil.copy2(src_path, dst_path)
