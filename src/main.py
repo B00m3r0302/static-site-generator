be6795686873f08e1d9cc85d60b083d4a4a19e3c
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-12-21 11:46:07 -0500

    page generation works

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import re
+from markdown_mod import markdown_to_html_node
+from htmlnode import ParentNode
 
 
 def copy_to(source, destination):
@@ -31,8 +33,30 @@ def extract_title(markdown):
         return header.group(1).strip()
 
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f_file:
+        from_content = f_file.read()
+
+    with open(template_path, "r") as t_file:
+        template_content = t_file.read()
+
+    # convert markdown to html
+    html_string = markdown_to_html_node(from_content).to_html()
+    title = extract_title(from_content)
+    template_content = template_content.replace("{{ Title }}", title)
+    template_content = template_content.replace("{{ Content }}", html_string)
+
+    # create parent directories if they don't exist
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    with open(dest_path, "w") as dest_file:
+        dest_file.write(template_content)
+
+
 def main():
     copy_to("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
