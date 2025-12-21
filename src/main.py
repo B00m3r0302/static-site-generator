6ec60c5edb2a16ec665aa4ea032d10985e06fa0d
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-12-21 12:06:22 -0500

    complete for github pages

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import re
+import sys
 from markdown_mod import markdown_to_html_node
 from htmlnode import ParentNode
 
@@ -33,7 +34,7 @@ def extract_title(markdown):
         return header.group(1).strip()
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f_file:
         from_content = f_file.read()
@@ -47,6 +48,10 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html_string)
 
+    # replace paths with basepath
+    template_content = template_content.replace('href="/', f'href="{basepath}')
+    template_content = template_content.replace('src="/', f'src="{basepath}')
+
     # create parent directories if they don't exist
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
@@ -54,23 +59,27 @@ def generate_page(from_path, template_path, dest_path):
         dest_file.write(template_content)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for item in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, item)
 
         if os.path.isdir(src_path):
             # recursively process subdirectories
             dest_subdir = os.path.join(dest_dir_path, item)
-            generate_pages_recursive(src_path, template_path, dest_subdir)
+            generate_pages_recursive(src_path, template_path, dest_subdir, basepath)
         elif item.endswith('.md'):
             # convert .md extension to .html for destination
             html_filename = item[:-3] + '.html'
             dest_path = os.path.join(dest_dir_path, html_filename)
-            generate_page(src_path, template_path, dest_path)
+            generate_page(src_path, template_path, dest_path, basepath)
 
 
 def main():
-    generate_pages_recursive("content", "template.html", "public")
+    # get basepath from CLI argument, default to /
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
+    copy_to("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
